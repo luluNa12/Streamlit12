44be15b5ab5822e4e320a226d4320c8f4dfccd02
luluNa12 <121836520+luluNa12@users.noreply.github.com>
2025-07-30 15:02:29 -0700

    Update Uber_NYC.py

--- Uber_NYC.py
+++ Uber_NYC.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC - \
-Lina was here')
+st.title('Uber pickups in NYC\nLina was here')
+
 
 st.write('Hello, * World! * :sunglasses:')
 st.write('Hello, * World! * :heart:')
